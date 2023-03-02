8b27cf398d293bede97421711e86fa60b9a5e967
Malek-Abdelal <malekbassam1144@gmail.com>
2023-03-02 14:47:24 +0300

    Adding js2 files

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
 var userName = prompt("Please inter your name:");
 var gender = prompt("Please inter your gender: (male / female)");
 var age = prompt("Please inter your age:");
+let visit = prompt("Is this your first time visiting this website ?");
+let field = prompt("Are you a programmer ?");
+let language = prompt("Do you familiar with JavaScript language ?");
 
 while (age <= 0) {
     alert("Your age is not correct, please inter a valid age.");
@@ -13,3 +16,25 @@ var welcMessage = confirm("Do you want to get the welcoming message ?");
 if (welcMessage == true) {
     alert((gender === "male") ? `Welcome, Mr ${userName}` : (gender === "female") ? `Welcome, Ms ${userName}` : `Welcome, ${userName}`);
 }
+
+let ansValidate = (answer1, answer2, answer3) => {
+
+    let answers = [answer1, answer2, answer3];
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i] === "") {
+            answers[i] = "invalid";
+        }
+    }
+    return answers;
+}
+
+let normalizedArr = ansValidate(visit, field, language);
+
+let printAnswers = (array) => {
+
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+}
+
+printAnswers(normalizedArr);
